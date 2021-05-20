c601e6e5a13829311693d63aa13a372fd254b26e
GitHub Action <action@github.com>
2021-05-20 13:01:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/kishanthan/exampleintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kishanthan.exampleintellijplugin.services
+
+import com.github.kishanthan.exampleintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/kishanthan/exampleintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kishanthan/exampleintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kishanthan.exampleintellijplugin.services
 
+import com.github.kishanthan.exampleintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
